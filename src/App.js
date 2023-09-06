a5376e978b1378868f5cd87372522ffaf4380ed9
Ali Uzunkaya <aliuzunkaya94@gmail.com>
2023-09-06 17:46:43 +0300

    react ödevidir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CategoryList from "./components/CategoryList";
+import TaskList from "./components/TaskList";
+import { categories, tasks } from "./data"; // data.js'den verileri içe aktar
 
 function App() {
+  // Kategorileri ve görevleri state olarak tanımlayın
+  const [categoryList, setCategoryList] = useState(categories);
+  const [taskList, setTaskList] = useState(tasks);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // Kategori ekleme işlevi
+  const addCategory = (name) => {
+    if (name.trim() === "") return;
+    const newCategory = {
+      id: Date.now(),
+      name,
+    };
+    setCategoryList([...categoryList, newCategory]);
+  };
+
+  // Kategori silme işlevi
+  const removeCategory = (categoryId) => {
+    const updatedCategories = categoryList.filter(
+      (category) => category.id !== categoryId
+    );
+    setCategoryList(updatedCategories);
+  };
+
+  // Seçili kategoriyi güncelleme işlevi
+  const selectCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
+  // Görev ekleme işlevi
+  const addTask = (name) => {
+    if (name.trim() === "") return;
+    const newTask = {
+      id: Date.now(),
+      name,
+      categoryId: selectedCategory.id,
+    };
+    setTaskList([...taskList, newTask]);
+  };
+
+  // Görev silme işlevi
+  const removeTask = (taskId) => {
+    const updatedTasks = taskList.filter((task) => task.id !== taskId);
+    setTaskList(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <CategoryList
+        categories={categoryList}
+        onAddCategory={addCategory}
+        onRemoveCategory={removeCategory}
+        onSelectCategory={selectCategory}
+      />
+      <TaskList
+        tasks={
+          selectedCategory
+            ? taskList.filter((task) => task.categoryId === selectedCategory.id)
+            : []
+        }
+        onAddTask={addTask}
+        onRemoveTask={removeTask}
+        selectedCategory={selectedCategory}
+      />
     </div>
   );
 }
